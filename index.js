5aefff558b8cf69618c02ee3c1e104314c057725
Jahray0 <jaegle.maxime@gmail.com>
2023-10-27 11:19:10 +0200

    test

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
+app.get("/test", (req, res) => {
+  res.send("Test on Vercel");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-module.exports = app;// Export the Express app
+module.exports = app; // Export the Express app
